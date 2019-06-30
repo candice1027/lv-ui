da24d9189bbd669035e3a96894e686d3038c86c3
lvsuzhen <1205521940@qq.com>
2019-06-30 19:14:56 +0800

    增加input组件

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 
+Vue.component('g-input',Input)
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
@@ -15,75 +17,4 @@ new Vue({
 
 
 
-//单元测试
-import chai from 'chai';
-import spies from 'chai-spies'
-const expect = chai.expect;
-chai.use(spies)
 
-//icon设置
-{
-    const constructor = Vue.extend(Button);
-    const button = new constructor({
-        propsData: {
-            icon: 'setting',
-        }
-    });
-    button.$mount()
-    let useElement = button.$el.querySelector('use');
-    expect(useElement.getAttribute('xlink:href')).to.eq('#i-setting')
-    button.$el.remove();
-    button.$destroy();
-}
-//loading
-{
-    const constructor = Vue.extend(Button);
-    const button = new constructor({
-        propsData: {
-            icon: 'setting',
-            loading: true
-        }
-    });
-    button.$mount()
-    let useElement = button.$el.querySelector('use');
-    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
-    button.$el.remove();
-    button.$destroy();
-}
-//按钮是否在右边
-{
-    const constructor = Vue.extend(Button);
-    const button = new constructor({
-        propsData: {
-            icon: 'setting',
-            iconLocate: 'right'
-        }
-    });
-    button.$mount('#test')
-    let svg = button.$el.querySelector('svg');
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq('2')
-}
-//button click事件
-{
-    const constructor = Vue.extend(Button);
-    const vButton = new constructor({
-        propsData: {
-            icon: 'setting'
-        }
-    })
-    vButton.$mount();
-
-    // vButton.$on('click',function(){
-    //     expect('1').to.eq('1')
-    // })
-    // console.log(vButton)
-    // let vBtn = vButton.$el;
-    // vBtn.click()
-    //使用chai-spy改写上面的
-    let spy = chai.spy(function(){})
-    vButton.$on('click',spy)
-    let vBtn = vButton.$el;
-    vBtn.click();
-    expect(spy).have.been.to.called();
-}
---
+++ src/input.vue
@@ -0,0 +1,87 @@
+<template>
+ <div class="wrapper" :class="{'error': error}">
+    <input type="text" :value="value" :disabled="disabled" :readonly="readonly">
+    <template v-if="error">
+        <Icon iconName="info" class="error-icon"></Icon>
+        <span class="error-message">{{error}}</span>
+    </template>
+    
+ </div>
+   
+</template>
+<script>
+import Icon from './icon';
+export default {
+    props: {
+        value: {
+            type:String, 
+        }, 
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        readonly: {
+            type: Boolean,
+            default: false
+        },
+        error: {
+            type: String
+        }
+    },
+    components: {
+        Icon
+    }
+
+    // props: ['value']
+    
+}
+</script>
+<style lang="scss" scoped>
+$height: 32px;
+$border-color: #999;
+$border-hover-color: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0,0,0,.5);
+$errorColor: #F1453D;
+.wrapper {
+    font-size: $font-size;
+    display: inline-flex;
+    align-items: center;
+    > :not(last-child){margin-right: .5em;}
+    > input {
+        font-size: inherit;
+        height: $height;
+        border: 1px solid $border-color;
+        padding: 0 8px;
+        border-radius: $border-radius;
+        &:hover {
+            border-color: $border-hover-color
+        }
+        &:focus {
+            outline: none;
+            box-shadow: inset 0 1px 3px $box-shadow-color;
+        }
+        &[disabled],&[readonly] {
+            color: #ccc;
+            border-color: #ccc;
+            cursor: not-allowed;
+        }
+
+    }
+    &.error {
+        > input {
+            border-color: $errorColor;
+        }
+    }
+    .error-icon {
+        fill: $errorColor;
+    }
+    .error-message {
+        color: $errorColor;
+    }
+}
+
+</style>
+
+
