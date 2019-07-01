4f5d1ce0e1b0096958751166491ab5a55bc48224
lvsuzhen <1205521940@qq.com>
2019-07-01 23:03:30 +0800

    增加row

---
+++ src/col.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="col" 
+    :class="[`col-${span}`,offset&&`offset-${offset}`]"
+    :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}"
+    >
+        <div style="border: 1px solid green;">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        span: {
+            type: [Number, String]
+        },
+        offset:{
+            type: [Number,String]
+        },
+        gutter: {
+            type: [Number,String]
+        }
+    },
+    data() {
+        return {
+           //gutter: 0 
+        }
+    }
+    
+}
+</script>
+<style lang="scss" scoped>
+.col {
+    width: 50%;
+    height: 50px;
+    $class-prefix: col-;
+    $offset-prefix: offset-;
+    @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+            width: ($n / 24) * 100%
+        }
+         &.#{$offset-prefix}#{$n} {
+            margin-left: ($n / 24) * 100%
+        }
+    }
+}
+
+</style>
+
+
---
+++ src/row.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row" 
+    :style="{marginLeft: -gutter/2+'px',marginRight:-gutter/2+'px'}"
+    >
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        gutter: {
+            type: [Number,String]
+        }
+    },
+    // data(){
+    //     return {
+    //       gutter: 0  
+    //     }
+    // },
+    mounted(){
+        this.$children.forEach((vm) => {
+            vm.gutter = this.gutter;
+        })
+    }  
+}
+</script>
+<style lang="scss" scoped>
+.row {
+    display: flex;
+    // margin: 0 -10px;
+    // border: 1px solid blue;
+}
+</style>
+
+
