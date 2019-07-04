b3b7d7d7c1a9acb8953d3d2f6766e0d68204973c
lvsuzhen <1205521940@qq.com>
2019-07-04 22:14:10 +0800

    基本布局组件

---
+++ src/layout.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="layout" :class="layoutClass">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            layoutClass: {
+                hasSider: false
+            }
+        }
+    },
+    mounted() {
+        this.$children.forEach(vm => {
+            if (vm.$options.name === 'sider-ui') {
+                this.layoutClass.hasSider = true;
+                console.log('sider-ui')
+            }
+        })
+        console.log(this.$children)
+    }
+    
+}
+</script>
+<style lang="scss" scoped>
+.layout {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    &.hasSider {
+        flex-direction: row;
+    }
+}
+
+
+</style>
+
+
---
+++ src/sider.vue
@@ -0,0 +1,34 @@
+<template>
+    <transition name="fade">
+        <div class="sider" v-if="show">
+            <slot></slot>
+            <button @click="hide">隐藏</button>
+        </div>
+    </transition> 
+</template>
+<script>
+export default {
+    name: 'sider-ui',
+    data() {
+        return {
+            show: true
+        } 
+    },
+    methods:{
+        hide() {
+            this.show = false;
+        }
+    }  
+}
+</script>
+<style lang="scss" scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: all .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  margin-left: -200px;
+}
+
+</style>
+
+
