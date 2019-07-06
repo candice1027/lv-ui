0b4cc70ca7dd374db1f352146fd860cef9acb3dd
lvsuzhen <1205521940@qq.com>
2019-07-06 16:39:42 +0800

    解决连续点击生成多个toast的功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,25 @@
 import Toast from './toast'
+let currentToast;
 export default {
     install(Vue,options) {
         console.log('我是toast')
         Vue.prototype.$toast = function(message,toastoptions) {
-           let Constructor = Vue.extend(Toast);
-           let toast = new Constructor({
-               propsData: toastoptions
-               
-           });
-           toast.$slots.default = [message]; 
-           toast.$mount(); 
-           document.body.appendChild(toast.$el)
+            if (currentToast) {
+                currentToast.closeToast();
+            }
+            currentToast = createToast({Vue,message,propsData:toastoptions})
         }
     }
+}
+
+function createToast({Vue,message,propsData}) {
+    let Constructor = Vue.extend(Toast);
+    let toast = new Constructor({
+        propsData
+        
+    });
+    toast.$slots.default = [message]; 
+    toast.$mount(); 
+    document.body.appendChild(toast.$el)
+    return toast;
 }
