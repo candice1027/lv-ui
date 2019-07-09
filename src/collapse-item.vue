05c6dfc088cd3867bbd615e3340b7b1dd14845b5
lvsuzhen <1205521940@qq.com>
2019-07-09 20:53:46 +0800

    完成手风琴组件的基本样式和功能

---
+++ src/collapse-item.vue
@@ -0,0 +1,62 @@
+
+<template>
+<div class="collapse-item">
+    <div class="title" @click="isOpen=!isOpen">{{title}}</div>
+    <div class="content" v-show="isOpen">
+        <slot></slot>
+    </div> 
+</div>
+ 
+</template>
+<script>
+export default {
+    name: 'collapse',
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            isOpen: false
+        }
+    }
+    
+}
+</script>
+<style lang="scss" scoped>
+$border-color: #ddd;
+$border-radius: 4px;
+.collapse-item {
+    > .title {
+        border: 1px solid $border-color;
+        margin-top: -1px;
+        margin-left: -1px;
+        margin-right: -1px;
+        min-height: 32px;
+        display: flex;
+        align-items: center;
+        padding: 0 8px;
+    }
+    &:first-child{
+        > .title {
+            border-top-left-radius: $border-radius;
+           border-top-right-radius: $border-radius;
+        }
+    }
+    &:last-child{
+        > .title:last-child {
+           border-bottom-left-radius: $border-radius;
+           border-bottom-right-radius: $border-radius;
+        }
+    }
+    > .content {
+        padding: 8px;
+    }
+   
+}
+
+</style>
+
+
---
+++ src/collapse.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'collapse'  
+}
+</script>
+<style lang="scss" scoped>
+$border-color: #ddd;
+$border-radius: 4px;
+.collapse {
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    // border-bottom: none;
+}
+
+</style>
+
+
