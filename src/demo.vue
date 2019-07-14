781e51f62d8de5c901ad78b4f0ff6f11dd3b561b
lvsuzhen <1205521940@qq.com>
2019-07-14 17:28:18 +0800

    改用vue-cli来构建项目

---
+++ src/demo.vue
@@ -0,0 +1,114 @@
+
+<template>
+<div>
+    <Button>按钮</Button>
+    <Cascader :source="source" popover-height="200px">
+                这里是级联选择器
+    </Cascader>
+</div>
+    
+</template>
+<script>
+import Button from './button'
+import CascaderItem from './cascader-item'
+import Cascader from './cascader'
+export default {
+    data(){
+        return {
+             source:[
+            {
+                name:'浙江',
+                children:[
+                    {
+                     name: '杭州',
+                      children: [
+                        {
+                            name:'上城区'
+                        },
+                        {
+                            name:'下城区'
+                        },
+                        {
+                            name:'江干区'
+                        },
+                    ]
+                    },
+                    {
+                        name: '嘉兴',
+                        children: [
+                            {
+                                name:'南湖区'
+                            },
+                            {
+                                name:'秀洲区'
+                            },
+                            {
+                                name:'嘉善县'
+                            },
+                        ]
+                    }
+                ]
+            },
+            {
+                name:'福建',
+                children:[
+                    {
+                     name: '福州',
+                      children: [
+                        {
+                            name:'鼓楼区'
+                        },
+                        {
+                            name:'台江区'
+                        },
+                        {
+                            name:'苍山区'
+                        },
+                    ]
+                    },
+                ]
+            },
+            {
+                name:'上海',
+                children:[
+                    {
+                     name: '上海',
+                      children: [
+                        {
+                            name:'浦东区'
+                        },
+                        {
+                            name:'徐汇区'
+                        },
+                        {
+                            name:'虹口区'
+                        },
+                    ]
+                    },
+                ]
+            }
+        ]
+
+        }
+       
+    },
+    components:{
+        Button,
+        Cascader,
+        CascaderItem
+    }
+    
+}
+</script>
+<style lang="scss" scoped>
+*{margin: 0;padding: 0;box-sizing: border-box;}
+:root {
+    --font-size: 14px;
+}
+body{
+    font-size: var(--font-size)
+}
+
+</style>
+
+
